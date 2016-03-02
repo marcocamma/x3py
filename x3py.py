753275a2614bc08f022238ff67d2fa02f4da4fa8
marco cammarata <marcocamma@gmail.com>
2016-03-03 00:08:36 +0100

    New matchTimestamp method of Dataset

--- x3py.py
+++ x3py.py
@@ -1,11 +1,29 @@
 """ This is a light version of the x3py package. Can simply acess the
    data """
 from x3py.toolsConf import config
+from x3py import toolsMatchTimeStamps,toolsVarious
 import x3py.lclsH5
+import numpy as np
 
 class Dataset(x3py.lclsH5.H5):
+  config = config
   def __init__(self,
     inputFilesOrExpRunTuple='',
+    matchTimeStamps=True,
     detectors = [],
     ):
     x3py.lclsH5.H5.__init__(self,inputFilesOrExpRunTuple)
+    self.detectors = config.detectors
+    if matchTimeStamps: self._matchTimeStamps()
+
+  def _matchTimeStamps(self,detectorList=None):
+    c=toolsVarious.CodeBlock("Time stamp matching started ...")
+    if detectorList is None:
+      dets = self.detectors.values()
+    else:
+      dets = detectorList
+    idx = toolsMatchTimeStamps.matchTimeStamps(*dets,returnCommonTimeStamp=False)
+    for (d,f) in zip(dets,idx):
+      d.defineFilter(f)
+    c.done()
+    
