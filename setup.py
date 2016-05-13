20711ab5ff171f416643f09f402a6a2e77afb382
marco cammarata <marcocamma@gmail.com>
2016-05-13 23:11:42 +0200

    better setup.py to compile cython extension

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     ext_modules=cythonize("matchTimeStamps.pyx"),
+    include_dirs=[numpy.get_include()]
 )
 
